d42d30603a4ec66c9db69af65ce172065d3a801f
Felix Arntz <felix-arntz@t-online.de>
2016-11-14 23:10:45 +0100

    introduce a filter in pluggable functions to support addon for global admin functionality

--- wp-network-roles/pluggable.php
+++ wp-network-roles/pluggable.php
@@ -10,19 +10,24 @@
 function wp_set_current_user( $id, $name = '' ) {
 	global $current_user;
 
+	$user_class_name = apply_filters( 'wpnr_user_class_name', 'WP_User_With_Network_Roles' );
+	if ( ! is_subclass_of( $user_class_name, 'WP_User_With_Network_Roles' ) ) {
+		$user_class_name = 'WP_User_With_Network_Roles';
+	}
+
 	// If `$id` matches the user who's already current, there's nothing to do.
 	if ( isset( $current_user )
 		&& ( $current_user instanceof WP_User )
 		&& ( $id == $current_user->ID )
 		&& ( null !== $id )
 	) {
-		if ( ! $current_user instanceof WP_User_With_Network_Roles ) {
-			$current_user = new WP_User_With_Network_Roles( $current_user );
+		if ( ! is_a( $current_user, $user_class_name ) ) {
+			$current_user = new $user_class_name( $current_user );
 		}
 		return $current_user;
 	}
 
-	$current_user = new WP_User_With_Network_Roles( $id, $name );
+	$current_user = new $user_class_name( $id, $name );
 
 	setup_userdata( $current_user->ID );
 
@@ -43,7 +48,12 @@ function get_user_by( $field, $value ) {
 		return false;
 	}
 
-	$user = new WP_User_With_Network_Roles;
+	$user_class_name = apply_filters( 'wpnr_user_class_name', 'WP_User_With_Network_Roles' );
+	if ( ! is_subclass_of( $user_class_name, 'WP_User_With_Network_Roles' ) ) {
+		$user_class_name = 'WP_User_With_Network_Roles';
+	}
+
+	$user = new $user_class_name;
 	$user->init( $userdata );
 	// The following is required to initialize extra network functionality.
 	$user->for_network();
