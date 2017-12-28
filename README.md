c845993c0805d5ef3995b10fdb49530f5d386006
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-12-28 15:35:02 +0100

    Synchronize super admins to the administrator network role.

--- README.md
+++ README.md
@@ -6,26 +6,25 @@
 
 # WP Network Roles
 
-Proof-of-Concept for implementing a real network-wide role system in WordPress Core.
+Implements network-wide user roles in WordPress.
 
 ## What it does
 
 * introduces classes `WP_Network_Role` and `WP_Network_Roles` for managing network-wide roles
-* enhances the `WP_User` class (by providing a `WP_User_With_Network_Roles` class which would become part of `WP_User` if the functionality was in Core)
-* uses user meta to store network-wide roles and capabilities (in a similar fashion like it stores site roles and capabilities)
+* introduces internal class `WPNR_User_With_Network_Roles` that stores network roles and capabilities for a user
+* introduces relationships between users and networks
 * allows to query users for specific network roles or whether they are part of a network
-* adjusts the network users list table to contain role filters and a column
+* uses user meta to store network-wide roles and capabilities, in a similar fashion like WordPress core stores site roles and capabilities)
 * introduces one initial network role "administrator"
-* automatically migrates the "super admins" stored in the network option into the user meta-based system
-* supports the WP Multi Network plugin when switching networks
+* synchronizes and migrates super admins to the network role "administrator"
+* adds network relationships to existing users
+* supports the WP Multi Network plugin
 
 ## How to install
 
 The plugin can either be installed as a network-wide regular plugin or alternatively as a must-use plugin.
 
-## Some background information
+## Recommendations
 
-The long-term plan is to only use the currently existing super-admin functionality for special access to everything (by providing the super admins with the `$super_admins` global). For network administrators (and possibly global administrators) dedicated role systems, similar to how they exist for site administrators, should be implemented. This plugin is a first take on this.
-
-* Roadmap document: https://docs.google.com/document/d/1MWwwKmhBJookr5dEcYga4sBtCwvx-K8uSucBFx6SP9U/edit?usp=sharing
-* Background discussion on Slack: https://wordpress.slack.com/archives/core-multisite/p1470762377000454 and https://wordpress.slack.com/archives/core-multisite/p1478204143000046
+* Do not set any of the following global variables: `$super_admins`, `$wp_user_roles`, `$wp_network_user_roles`
+* Do not rely on `is_super_admin()`, but rather on actual capabilities
---
+++ wp-network-roles/wp-includes/sync-super-admins.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Functions to synchronize the super admins network option with the Administrator network role.
+ *
+ * @package WPNetworkRoles
+ * @since 1.0.0
+ */
+
+/**
+ * Ensures user metadata is used to get the super admins.
+ *
+ * This function should be unhooked, if you consider super admins to actually
+ * be super admins, not network administrators. This is not the common
+ * understanding though.
+ *
+ * @since 1.0.0
+ * @access private
+ *
+ * @param mixed  $pre        Value to override.
+ * @param string $option     Option name.
+ * @param int    $network_id Network ID.
+ * @param mixed  $default    Default option value.
+ * @return array Array of network administrator user names.
+ */
+function _nr_filter_super_admins( $pre, $option, $network_id, $default ) {
+	$users = get_users( array(
+		'blog_id'      => 0,
+		'network_id'   => $network_id,
+		'network_role' => 'administrator',
+	) );
+
+	if ( empty( $users ) ) {
+		return $default;
+	}
+
+	return wp_list_pluck( $users, 'user_login' );
+}
+add_filter( 'pre_site_option_site_admins', '_nr_filter_super_admins', 10, 4 );
+
+/**
+ * Ensures granting super admin users on a new network automatically receive the 'administrator' network role.
+ *
+ * @since 1.0.0
+ * @access private
+ *
+ * @global array $wpnr_users_with_network_roles Internal storage for user objects with network roles.
+ *
+ * @param array $network_options All network options for the new network.
+ * @param int   $network_id      ID of the new network.
+ * @return array Unmodified network options.
+ */
+function _nr_set_network_administrators_on_new_network( $network_options, $network_id ) {
+	global $wpnr_users_with_network_roles;
+
+	if ( ! empty( $network_options['site_admins'] ) ) {
+		$network_id = (int) $network_id;
+
+		$users = get_users( array(
+			'blog_id'   => 0,
+			'login__in' => $network_options['site_admins'],
+		) );
+
+		foreach ( $users as $user ) {
+			if ( ! isset( $wpnr_users_with_network_roles[ $user->ID ] ) ) {
+				$wpnr_users_with_network_roles[ $user->ID ] = new WPNR_User_With_Network_Roles( $user->ID, $network_id );
+			} elseif ( $wpnr_users_with_network_roles[ $user->ID ]->get_network_id() !== $network_id ) {
+				$wpnr_users_with_network_roles[ $user->ID ]->for_network( $network_id );
+			}
+
+			$wpnr_users_with_network_roles[ $user->ID ]->add_network_role( 'administrator' );
+		}
+	}
+
+	return $network_options;
+}
+add_filter( 'populate_network_meta', '_nr_set_network_administrators_on_new_network', 10, 2 );
+
+/**
+ * Ensures granting super admin privileges adds the 'administrator' network role to that user.
+ *
+ * This function should be unhooked, if you consider super admins to actually
+ * be super admins, not network administrators. This is not the common
+ * understanding though.
+ *
+ * @since 1.0.0
+ * @access private
+ *
+ * @global array $wpnr_users_with_network_roles Internal storage for user objects with network roles.
+ *
+ * @param int $user_id User ID of the user who was granted super admin privileges.
+ */
+function _nr_grant_network_administrator( $user_id ) {
+	global $wpnr_users_with_network_roles;
+
+	if ( ! isset( $wpnr_users_with_network_roles[ $user_id ] ) ) {
+		$wpnr_users_with_network_roles[ $user_id ] = new WPNR_User_With_Network_Roles( $user_id );
+	}
+
+	$wpnr_users_with_network_roles[ $user_id ]->add_network_role( 'administrator' );
+}
+add_action( 'granted_super_admin', 'wpnr_grant_network_administrator', 10, 1 );
+
+/**
+ * Ensures revoking super admin privileges removes the 'administrator' network role from that user.
+ *
+ * This function should be unhooked, if you consider super admins to actually
+ * be super admins, not network administrators. This is not the common
+ * understanding though.
+ *
+ * @since 1.0.0
+ * @access private
+ *
+ * @global array $wpnr_users_with_network_roles Internal storage for user objects with network roles.
+ *
+ * @param int $user_id User ID of the user whose super admin privileges were revoked.
+ */
+function _nr_revoke_network_administrator( $user_id ) {
+	global $wpnr_users_with_network_roles;
+
+	if ( ! isset( $wpnr_users_with_network_roles[ $user_id ] ) ) {
+		$wpnr_users_with_network_roles[ $user_id ] = new WPNR_User_With_Network_Roles( $user_id );
+	}
+
+	$wpnr_users_with_network_roles[ $user_id ]->remove_network_role( 'administrator' );
+}
+add_action( 'revoked_super_admin', 'wpnr_revoke_network_administrator', 10, 1 );
