e802e304d9136cf45cf6f81009724882f6b35e70
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-01-21 18:13:54 +0100

    Add compatibility with multi network to set new network administrators on new network.

--- wp-network-roles/multi-network-compat.php
+++ wp-network-roles/multi-network-compat.php
@@ -26,3 +26,21 @@ function _nr_switched_network( $new_network_id, $old_network_id ) {
 	$nr_current_user->for_network( $new_network_id );
 }
 add_action( 'switch_network', '_nr_switched_network', 10, 2 );
+
+/**
+ * Ensures the network administrator roles are set correctly when a new network is created with WPMN.
+ *
+ * @since 1.0.0
+ *
+ * @param int $network_id New network ID.
+ */
+function _nr_set_network_administrators_on_wpmn_add_network( $network_id ) {
+	remove_filter( 'pre_site_option_site_admins', '_nr_filter_super_admins', 10 );
+	$network_options = array(
+		'site_admins' => get_network_option( $network_id, 'site_admins', array() ),
+	);
+	add_filter( 'pre_site_option_site_admins', '_nr_filter_super_admins', 10, 4 );
+
+	_nr_set_network_administrators_on_new_network( $network_options, $network_id );
+}
+add_action( 'add_network', '_nr_set_network_administrators_on_wpmn_add_network', 10, 1 );
